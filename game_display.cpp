117f985f7c19a133128ae2c1d2cc8a5709125b8f
ZerglingOne <jholcomb87@gmail.com>
2017-04-02 15:51:49 -0700

    Create game_display.cpp

---
+++ game_display.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "grid.h"
+
+void drawBoard(int states[64])
+{
+  for(int i = 0; i < 8; i++)//determines rows
+    {
+      for(int j = 0; j < 5; j++)//the lines of the boxes
+      {
+        for(int k = 0; k < 8; k++)//determines columns.
+      }
+    }
+}
